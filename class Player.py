b5e21bf2a3b658dee16b60ac4e3e2f4e12440113
Xinyi Chen <xinyic2@andrew.cmu.edu>
2022-11-18 13:51:19 -0500

    11.18 first commit

--- class Player.py
+++ class Player.py
@@ -1,3 +1,155 @@
 #commit 
+import random
+
+npNameList = ['Acadia', 'American Samoa', 'Arches', 'Badlands', 'Big Bend',
+            'Biscayne', 'Carlsbad Caverns', 'Crater Lake', 'Death Valley',
+            'Dry Tortugas', 'Gates of the Arctic', 'Glacier Bay',
+            'Great Smoky Mountains', 'Isle Royale', 'Joshua Tree',
+            'Kings Canyon', 'Lake Clark', 'New River Gorge', 'North Cascades',
+            'Petrified Forest', 'Redwood', 'Rocky Mountain', 'Saguaro',
+            'Sequoia', 'Theodore Roosevelt', 'Voyageurs', 'Virgin Islands',
+            'White Sands', 'Yellowstone', 'Zion']
+cpNameList = ['Royal Gorge Park', 'Falls Park', 'Scioto Audubon',
+              'Rifle Mountain Park', 'Fairmount Park', 'City Park',
+              'Zilker Park', 'The Gathering Place', 'Papago Park']
+nameList = npNameList + cpNameList
+
+# print(len(nameList))
+class Grid:
+    def __init__(self):
+        self.name = random.choice(nameList)
+        nameList.remove(self.name)
+        self.owner = []
+        self.priceToBuy = random.randint(3000, 6000)
+        self.level = 0
+        self.toll = (random.randint(0.3, 0.7) * self.priceToBuy * 
+                     (self.level + 1))
+    
+    def buyProperty(self, player):
+        self.owner = player
+        self.level = 1
+    
+    def upgradeProperty(self):
+        self.level += 1
+        
+    
+
+# g1 = Grid()
+# print(g1.name)
+# print(nameList)
+
+
+
+
+
+#board
+board1 = [[0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+          [0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+          [0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+          [0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+          [0,0,0,0,0,0,0,0,0,1,1,1,0,0,1],
+          [1,1,1,1,1,1,1,1,1,1,0,0,0,0,1],
+          [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+          [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+          [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+          [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+          [1,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+          [1,1,1,0,0,0,0,0,0,1,0,0,0,0,0],
+          [0,0,1,0,0,0,0,0,0,1,0,0,0,0,0],
+          [0,0,1,1,1,1,1,1,1,1,0,0,0,0,0]
+]
+count = 0
+for row in range(len(board1)):
+    for col in range(len(board1[0])):
+        if board1[row][col]:
+            count += 1
+print(count)
+
+
+board2 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0.0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+
+board3 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0.0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+board4 = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0.0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+          [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+
+class Board(Grid):
+    def __init__(self, selectedBoard):
+        self.map = selectedBoard
+    
+    def getDims(self):
+        rows = len(self.map)
+        cols = len(self.map[0])
+        return rows, cols
+
+    def getRandomPlace(self): 
+        rows, cols = self.getDims()
+        if True:
+            x = random.randint(0, rows)
+            y = random.randint(0, cols)
+            if self.map[x][y]:
+                return x, y
+
+#testCase of getRandomPlace()
+myBoard = Board(board1)
+# print(myBoard.getRandomPlace())
+
+
+dice = random.randint(1, 6)
+
 class Player:
-    pass
+    def __init__(self):
+        self.loc = myBoard.getRandomPlace()
+        self.cards = []
+        self.myTurn = False
+    
+    def changeMyTurn(self):
+        self.myTurn = True
+    
